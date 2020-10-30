dc8f8c54fedc914dc436db848a164851bac86f24
Elvis Bueno <elvisbueno@elviss-mbp.attlocal.net>
2020-10-30 14:49:59 -0500

    code structure cleaned

--- google_maps/src/App.js
+++ google_maps/src/App.js
@@ -4,18 +4,26 @@ import './App.css';
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+
+
+
 function App() {
+
+  const API_KEY =`${process.env.REACT_APP_API_KEY}`
+  const URL_WITH_KEY = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`
 
   const MapWithAMarker = withScriptjs(withGoogleMap(props =>
     <GoogleMap defaultZoom={5} defaultCenter={{ lat: 36.55887395544204, lng: -87.4639293245037  }}>
     <Marker position={{ lat: 36.55887395544204, lng: -87.4639293245037 }}/>
     </GoogleMap>
     
+    
   ));
   
+  
 
-const API_KEY =`${process.env.REACT_APP_API_KEY}`
-const DATA = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`
+
 
 
 
@@ -23,11 +31,10 @@ const DATA = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&lib
     <div className='map'>
       <h1 className='title'>Hello World</h1>
 
-      <MapWithAMarker
-  googleMapURL= {DATA}
-  loadingElement={<div style={{ height: `100%` }} />}
-  containerElement={<div style={{ height: `500px` }} />}
-  mapElement={<div style={{ height: `100%` }} />}/>
+      <MapWithAMarker googleMapURL= {URL_WITH_KEY}
+                      loadingElement={<div style={{ height: `100%` }} />}
+                      containerElement={<div style={{ height: `500px` }} />}
+                      mapElement={<div style={{ height: `100%` }} />}/>
     </div>
   );
 }
